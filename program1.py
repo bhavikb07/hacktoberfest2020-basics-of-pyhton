50b2b40bd1f111011806db9108753613b9189084
Bhavik Bothra <43561956+bhavikb07@users.noreply.github.com>
2020-10-25 16:49:44 +0530

    Update program1.py
    Added difference and multiplication also

--- program1.py
+++ program1.py
@@ -11,3 +11,19 @@ sum = number1 + number2
 
 print("The sum of", number1, 'and', number2, 'is')
 print(sum)
+
+# Subtract two numbers
+diff = number1 - number2
+
+# Display the difference
+
+print("The difference of", number1, 'and', number2, 'is')
+print(diff)
+
+# Multiply two numbers
+mul = number1 * number2
+
+# Display the mul
+
+print("The product of", number1, 'and', number2, 'is')
+print(mul)
